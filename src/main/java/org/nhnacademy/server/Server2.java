75fad8460d6a255c148dacd3f05b4c131b9b8bd5
LeeJungBum <jungbum05114@gmail.com>
2023-10-16 04:30:35 +0900

    feat: server Quiz2->3

--- src/main/java/org/nhnacademy/server/Server2.java
+++ src/main/java/org/nhnacademy/server/Server2.java
@@ -14,25 +14,26 @@ public class Server2 {
         int port = 221;
         try {
             ServerSocket serverSocket = new ServerSocket(port);
+            while(!Thread.interrupted()) {
 
-            System.out.println("서버 소켓이 생성되어 클라이언트 접속을 기다린다");
-            Socket socket = serverSocket.accept();
+                System.out.println("서버 소켓이 생성되어 클라이언트 접속을 기다린다");
+                Socket socket = serverSocket.accept();
 
-            try {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
-                    writer.write(line);
-                    writer.newLine();
-                    writer.flush();
+                try {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        System.out.println(line);
+                        writer.write(line);
+                        writer.newLine();
+                        writer.flush();
+                    }
+                } catch (IOException ignore) {
+                } finally {
+                    socket.close();
                 }
-            } catch (IOException ignore) {
-            } finally {
-                socket.close();
             }
-
             serverSocket.close();
         } catch (IOException e) {
             System.out.println("지정된 포트[ " + port + " ]가 이미 사용중입니다.");
