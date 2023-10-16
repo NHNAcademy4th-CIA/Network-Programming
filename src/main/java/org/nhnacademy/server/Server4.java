127db710836093e5feaf8fb767557cf11a5a6b96
LeeJungBum <jungbum05114@gmail.com>
2023-10-16 14:24:52 +0900

    feat: server Quiz4

--- src/main/java/org/nhnacademy/server/Server4.java
+++ src/main/java/org/nhnacademy/server/Server4.java
@@ -5,35 +5,20 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server4 {
-    public static class Server extends Thread{
+    public static class Server extends Thread {
         private ServerSocket serverSocket;
 
-        public Server(int port)throws IOException{
-            this.serverSocket = new ServerSocket(port);
-        }
+        public Server(ServerSocket serverSocket){
+            this.serverSocket=serverSocket;
+         }
+
         @Override
-        public void run(){
-            try{
-
-            }catch (IOException e)
-            {
-
-            }
-        }
-        public static ServerSocket getServerSocket(){
-
-        }
-
-    }
-    public static void main(String[] args) {
-        int port = 221;
-        try {
-            while(!Thread.interrupted()) {
+        public void run() {
+            while (!Thread.interrupted()) {
 
                 System.out.println("서버 소켓이 생성되어 클라이언트 접속을 기다린다");
-                Socket socket = serverSocket.accept();
-
                 try {
+                    Socket socket = serverSocket.accept();
                     BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
                     String line;
@@ -43,15 +28,27 @@ public class Server4 {
                         writer.newLine();
                         writer.flush();
                     }
-                } catch (IOException ignore) {
-                } finally {
                     socket.close();
+                } catch (IOException ignore) {
+                    System.out.println(ignore);
                 }
             }
-            serverSocket.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        int port = 221;
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            Server server[] = new Server[2];
+            for (int i=0;i<2;i++) {
+                server[i]=new Server(serverSocket);
+                server[i].start();
+            }
         } catch (IOException e) {
             System.out.println("지정된 포트[ " + port + " ]가 이미 사용중입니다.");
         }
 
     }
+
 }
