486ff6b5575b3a8bd7f2d63e46749350ad1f97d0
minm063 <jangmin063@gmail.com>
2023-10-17 15:24:09 -0700

    refactor: isConnected 수정

--- src/main/java/org/nhnacademy/minju/client/problem1/Client.java
+++ src/main/java/org/nhnacademy/minju/client/problem1/Client.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
-public class Client extends Thread{
+public class Client extends Thread {
     private String host;
     private int port;
     private Socket socket;
@@ -20,16 +20,14 @@ public class Client extends Thread{
     public void run() {
         try {
             socket = new Socket(host, port);
-            if (socket.isConnected()) {
-                System.out.println("서버에 연결되었습니다.");
-            }
+            System.out.println("서버에 연결되었습니다.");
             socket.close();
         } catch (IOException e) {
             System.out.println("error : " + e.getMessage());
         }
     }
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         Client client = new Client("localhost", 65514);
         client.start();
 
