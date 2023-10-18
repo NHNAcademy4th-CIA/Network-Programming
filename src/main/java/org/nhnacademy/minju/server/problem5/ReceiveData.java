1eb6c8d23148a29101996332f6d891ee2b0ec028
minm063 <jangmin063@gmail.com>
2023-10-18 11:08:46 -0700

    refactor: serverList responsibility를 메인에서 분리

--- src/main/java/org/nhnacademy/minju/server/problem5/ReceiveData.java
+++ src/main/java/org/nhnacademy/minju/server/problem5/ReceiveData.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,11 +17,10 @@ public class ReceiveData extends Thread {
 
     private Socket socket;
     private BufferedWriter writer;
-    private List<ReceiveData> serverList;
+    private List<ReceiveData> serverList = new ArrayList<>();
 
-    public ReceiveData(Socket socket, List<ReceiveData> serverList) {
+    public ReceiveData(Socket socket) {
         this.socket = socket;
-        this.serverList = serverList;
         serverList.add(this);
     }
 
@@ -68,33 +66,4 @@ public class ReceiveData extends Thread {
         }
     }
 
-    public static void main(String[] args) {
-        List<ReceiveData> serverList = new ArrayList<>();
-        ServerSocket serverSocket = null;
-        int port = 1234;
-
-        try {
-            serverSocket = new ServerSocket(port);
-
-            System.out.println("서버 소켓이 생성되었습니다.");
-            while (!Thread.interrupted()) {
-                System.out.println("클라이언트 연결을 기다립니다.");
-
-                Socket socket = serverSocket.accept();
-                ReceiveData server = new ReceiveData(socket, serverList);
-                server.start();
-            }
-        } catch (IOException e) {
-            System.out.println("지정된 포트[ " + port + " ]가 이미 사용중입니다.");
-        } finally {
-            // 서버 소켓을 닫는다
-            if (serverSocket != null) {
-                try {
-                    serverSocket.close();
-                } catch (IOException e) {
-                    System.out.println("server socket error : " + e.getMessage());
-                }
-            }
-        }
-    }
 }
